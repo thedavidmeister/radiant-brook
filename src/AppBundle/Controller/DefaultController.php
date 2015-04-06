6336cc9f21f332b199e1ba3ca0c85c82473bb642
David Meister <thedavidmeister@gmail.com>
2015-04-06 23:28:12 +0800

    override index.html.twig with an AppBundle one

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('AppBundle::index.html.twig');
     }
 }
