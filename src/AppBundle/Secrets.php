862efd2daa0c7eee1dd39341ecd4b9a718d88c7a
David Meister <thedavidmeister@gmail.com>
2015-05-23 16:15:40 +1000

    using Dotenv in Secrets

--- src/AppBundle/Secrets.php
+++ src/AppBundle/Secrets.php
@@ -2,29 +2,30 @@
 
 namespace AppBundle;
 
+use Dotenv;
+
 /**
  * Handles things that need to be secret.
  */
 class Secrets
 {
+    public function __construct() {
+        Dotenv::load(__DIR__);
+    }
+
     /**
      * Extracts secret keys from the file system or environment variables.
      *
      * @param string $name
-     *   THe name of the secret to get.
+     *   The name of the secret to get.
      *
      * @return string
      *   Returns the secret if found.
      */
     public function get($name)
     {
-        // First try environment variables.
-        if (getenv($name)) {
-            return trim(getenv($name));
-        }
-
-        if (file_exists(__DIR__ . '/Resources/keys/' . $name)) {
-            return trim(file_get_contents(__DIR__ . '/Resources/keys/' . $name));
+        if ($value = getenv($name)) {
+            return trim($value);
         }
 
         throw new \Exception('Secret not found: ' . $name);
