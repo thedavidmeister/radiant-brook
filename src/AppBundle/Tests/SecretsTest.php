f5a2b35f2088e0df1bbfdeabb0438ad8d35c621f
David Meister <thedavidmeister@gmail.com>
2015-05-24 16:09:38 +1000

    lint

--- src/AppBundle/Tests/SecretsTest.php
+++ src/AppBundle/Tests/SecretsTest.php
@@ -5,32 +5,36 @@ namespace AppBundle\Tests;
 use AppBundle\Secrets;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Tests \AppBundle\Secrets
+ */
 class SecretsTest extends WebTestCase
 {
-  protected function secrets()
-  {
-    return new Secrets();
-  }
+    protected function secrets()
+    {
+        return new Secrets();
+    }
 
-  /**
-   * Tests that secrets can find variables in the environment.
-   *
-   * @group stable
-   */
-  public function testSecretsEnv()
-  {
-    putenv('FOOBAR=BINGBAZ');
-    $this->assertSame('BINGBAZ', $this->secrets()->get('FOOBAR'));
-  }
+    /**
+     * Tests that secrets can find variables in the environment.
+     *
+     * @group stable
+     */
+    public function testSecretsEnv()
+    {
+        putenv('FOOBAR=BINGBAZ');
+        $this->assertSame('BINGBAZ', $this->secrets()->get('FOOBAR'));
+    }
 
-  /**
-   * Tests exceptions thrown when secrets are not found.
-   *
-   * @expectedException Exception
-   * @expectedExceptionMessage Secret not found:
-   * @group stable
-   */
-  public function testSecretsException() {
-    $this->secrets()->get('no match');
-  }
+    /**
+     * Tests exceptions thrown when secrets are not found.
+     *
+     * @expectedException Exception
+     * @expectedExceptionMessage Secret not found:
+     * @group stable
+     */
+    public function testSecretsException()
+    {
+        $this->secrets()->get('no match');
+    }
 }
