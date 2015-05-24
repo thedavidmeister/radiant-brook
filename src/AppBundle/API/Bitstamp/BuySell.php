ef13eff6a867f8d0caa94e285d8cae38d4c3d862
David Meister <thedavidmeister@gmail.com>
2015-05-24 19:47:37 +1000

    lint

--- src/AppBundle/API/Bitstamp/BuySell.php
+++ src/AppBundle/API/Bitstamp/BuySell.php
@@ -4,35 +4,67 @@ namespace AppBundle\API\Bitstamp;
 
 use AppBundle\MoneyStrings;
 
+/**
+ * Convenience class to lodge a buy and sell pair simultaneously.
+ */
 class BuySell
 {
-  public function __construct(
-    PrivateAPI\Buy $buy,
-    PrivateAPI\Sell $sell,
-    \Psr\Log\LoggerInterface $logger
-  )
-  {
-    $this->buy = $buy;
-    $this->sell = $sell;
-    $this->logger = $logger;
-  }
+    /**
+     * DI constructor.
+     *
+     * @param PrivateAPI\Buy           $buy
+     * @param PrivateAPI\Sell          $sell
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(
+      PrivateAPI\Buy $buy,
+      PrivateAPI\Sell $sell,
+      \Psr\Log\LoggerInterface $logger
+    )
+    {
+        $this->buy = $buy;
+        $this->sell = $sell;
+        $this->logger = $logger;
+    }
 
-  const PRICE_KEY = 'price';
+    const PRICE_KEY = 'price';
 
-  const VOLUME_KEY = 'amount';
+    const VOLUME_KEY = 'amount';
 
-  public function execute(Money $bidPrice, Money $bidVolume, Money $askPrice, Money $askVolume) {
-      $this->buy
-      ->setParam(self::PRICE_KEY, MoneyStrings::USDToString($bidPrice))
-      ->setParam(self::VOLUME_KEY, MoneyStrings::BTCToString($bidVolume))
-      ->execute();
+    /**
+     * Executes a buy and a sell simultaneously.
+     *
+     * This function is totally naive in that, if either leg of the buy/sell fails
+     * it does not prevent the other leg from executing. It is entirely possible
+     * that a buy can place as a sell fails, or vice-versa.
+     *
+     * Basic handling of logging when trade pairs are placed.
+     *
+     * @param Money::USD $bidPrice
+     *   The bid price to place, in USD Money.
+     *
+     * @param Money::BTC $bidVolume
+     *   The ask price to place, in BTC Money.
+     *
+     * @param Money::USD $askPrice
+     *   The ask price to place, in USD Money.
+     *
+     * @param Money::BTC $askVolume
+     *   The ask volume to place, in BTC Money.
+     */
+    public function execute(Money $bidPrice, Money $bidVolume, Money $askPrice, Money $askVolume)
+    {
+        $this->buy
+            ->setParam(self::PRICE_KEY, MoneyStrings::USDToString($bidPrice))
+            ->setParam(self::VOLUME_KEY, MoneyStrings::BTCToString($bidVolume))
+            ->execute();
 
-    $this->sell
-      ->setParam(self::PRICE_KEY, MoneyStrings::USDToString($askPrice))
-      ->setParam(self::VOLUME_KEY, MoneyStrings::BTCToString($askVolume))
-      ->execute();
+        $this->sell
+            ->setParam(self::PRICE_KEY, MoneyStrings::USDToString($askPrice))
+            ->setParam(self::VOLUME_KEY, MoneyStrings::BTCToString($askVolume))
+            ->execute();
 
-      $this->logger->info('Trade pairs executed.');
-  }
+        $this->logger->info('Trade pairs executed.');
+    }
 
 }
