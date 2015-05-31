f5b96a4e5e83d63dfd0a105a3d5350b623a3c1f5
David Meister <thedavidmeister@gmail.com>
2015-05-31 13:27:43 +1000

    test class for snapshotbitstamp

---
+++ src/AppBundle/Tests/SnapshotBitstampTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Tests;
+
+use AppBundle\SnapshotBitstamp;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Tests \AppBundle\SnapshotBitsamp
+ */
+class SnapshotBitstamp extends WebTestCase
+{
+
+}
