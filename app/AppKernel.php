620534c1e2cd79f00f5d8c72faffa8bc9915087c
David Meister <thedavidmeister@gmail.com>
2015-07-26 14:08:46 +1000

    backing out sanitybundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle(),
-            new RadiantBrook\SanityBundle\RadiantBrookSanityBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/RadiantBrook/SanityBundle/RadiantBrookSanityBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace RadiantBrook\SanityBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class RadiantBrookSanityBundle extends Bundle
-{
-}
