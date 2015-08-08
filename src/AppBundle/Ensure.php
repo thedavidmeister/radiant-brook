856583ba58159d1d8da2e5ce59f6256fa1b9597d
David Meister <thedavidmeister@gmail.com>
2015-08-08 20:43:28 +1000

    in range tests

--- src/AppBundle/Ensure.php
+++ src/AppBundle/Ensure.php
@@ -25,15 +25,23 @@ final class Ensure {
         return (filter_var($value, FILTER_VALIDATE_INT) !== false) ? (int) $value : self::fail($value, $message);
     }
 
-    public static function inRange($value, $min, $max, $message = '%s is not in range.') {
-        return ($min <= $value && $value <= $max) ? $value : self::fail($value, $message);
+    public static function inRange($value, $bound_one, $bound_two, $message = '%s is not in the range of %s and %s.') {
+        $min = min([$bound_one, $bound_two]);
+        $max = max([$bound_one, $bound_two]);
+        if ($min <= $value && $value <= $max) {
+            return $value;
+        }
+        else {
+            self::fail($value, $message, [$value, $min, $max]);
+        }
     }
 
     public static function isInstanceOf($value, $class, $message = '%s is not an instance of %s.') {
         return ($value instanceof $class) ? $value : self::fail($value, $class, $message);
     }
 
-    public static function fail($value, $message) {
-        throw new \Exception(vsprintf($message, json_encode($value, true)));
+    public static function fail($value, $message, array $args = []) {
+        $json_args = array_map(function($arg) { return json_encode($arg, true); }, $args);
+        throw new \Exception(vsprintf($message, $json_args));
     }
 }
--- src/AppBundle/Tests/EnsureTest.php
+++ src/AppBundle/Tests/EnsureTest.php
@@ -143,4 +143,51 @@ class EnsureTest extends WebTestCase
     $this->setExpectedException('Exception', $message);
     Ensure::isInt($notInt);
   }
+
+  /**
+   * Tests inRange().
+   *
+   * @group stable
+   */
+  public function testInRange() {
+    $tests = [
+      [0, -1, 1],
+      [2, 1, 3],
+      [1.5, 1, 2],
+      // Ranges can go backwards too.
+      [1.5, 2, 1],
+      [2, 3, 1],
+      [0, 1, -1],
+    ];
+    array_walk($tests, function($test) {
+      $this->assertSame($test[0], Ensure::inRange($test[0], $test[1], $test[2]));
+    });
+  }
+
+  /**
+   * Data provider for testInRangeExceptions.
+   *
+   * @return array
+   */
+  public function dataInRangeExceptions() {
+    return [
+      [-1, 1, 0, '-1 is not in the range of 0 and 1.'],
+      [1, 3, 2, '1 is not in the range of 2 and 3.'],
+      [1, 2, 1.5, '1 is not in the range of 1.5 and 2.'],
+      [2, 1, 1.5, '2 is not in the range of 1 and 1.5.'],
+      [3, 2, 1, '3 is not in the range of 1 and 2.'],
+      [1, -1, 0, '1 is not in the range of -1 and 0.'],
+    ];
+  }
+
+  /**
+   * Tests exceptions for inRange().
+   *
+   * @dataProvider dataInRangeExceptions
+   * @group stable
+   */
+  public function testInRangeExceptions($value, $bound_one, $bound_two, $message) {
+    $this->setExpectedException('Exception', $message);
+    Ensure::inRange($value, $bound_one, $bound_two);
+  }
 }
