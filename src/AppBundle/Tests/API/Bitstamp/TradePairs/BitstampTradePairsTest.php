e2bd36eefb3ecbee54dcf134b375e2cf16d0e8c9
David Meister <thedavidmeister@gmail.com>
2015-08-23 13:46:47 +1000

    test some isFinal edge cases

--- src/AppBundle/Tests/API/Bitstamp/TradePairs/BitstampTradePairsTest.php
+++ src/AppBundle/Tests/API/Bitstamp/TradePairs/BitstampTradePairsTest.php
@@ -76,18 +76,25 @@ class BitstampTradePairsTest extends WebTestCase
         return new BitstampTradePairs($this->fees(), $this->dupes(), $this->buysell(), $this->proposer());
     }
 
-    protected function statefulProposalMock($isValid = false, $isCompulsory = false, $isFinal = false)
+    protected function statefulProposalMockRaw($isValid = false, $isCompulsory = false, $isFinal = false)
     {
         $proposal = $this->prophet->prophesize('\AppBundle\API\Bitstamp\TradePairs\TradeProposal');
         $proposal->isValid()->willReturn($isValid);
         $proposal->isCompulsory()->willReturn($isCompulsory);
         $proposal->isFinal()->willReturn($isFinal);
 
+        return $proposal;
+    }
+
+    protected function statefulProposalMock($isValid = false, $isCompulsory = false, $isFinal = false)
+    {
+        $proposal = $this->statefulProposalMockRaw($isValid, $isCompulsory, $isFinal);
+
         return $proposal->reveal();
     }
 
     protected function statefulProposalMockFiller ($isValid = false, $isCompulsory = false, $isFinal = false) {
-        return array_fill(0, mt_rand(0, 10), $this->statefulProposalMock($isValid));
+        return array_fill(0, mt_rand(0, 10), $this->statefulProposalMock($isValid, $isCompulsory, $isFinal));
     }
 
     protected function assertActionableReport(array $pre, array $post, array $tests)
@@ -128,6 +135,85 @@ class BitstampTradePairsTest extends WebTestCase
         } while ($i < 20);
     }
 
+    protected function randomBool() {
+        return (bool) mt_rand(0, 1);
+    }
+
+    /**
+     * @covers AppBundle\API\Bitstamp\TradePairs\BitstampTradePairs::reduceReportToActionableTradeProposal
+     *
+     * @group stable
+     */
+    public function testReduceReportToActionableTradeProposalInvalidFinal()
+    {
+        // Start with an invalid mock.
+        $first = $this->statefulProposalMockRaw(false);
+        // Should be checked.
+        $first->isValid()->shouldBeCalled();
+        $first->isCompulsory()->shouldBeCalled();
+        $first->isFinal()->shouldBeCalled();
+
+        // Then an invalid final.
+        $second = $this->statefulProposalMockRaw(false, false, true);
+        // Should be checked.
+        $second->isValid()->shouldBeCalled();
+        $second->isCompulsory()->shouldBeCalled();
+        $second->isFinal()->shouldBeCalled();
+
+        // Then whatever.
+        $third = $this->statefulProposalMockRaw($this->randomBool(), $this->randomBool(), $this->randomBool());
+        $third->isValid()->shouldNotBeCalled();
+        $third->isCompulsory()->shouldNotBeCalled();
+        $third->isFinal()->shouldNotBeCalled();
+
+        // Build the sequence.
+        $sequence = [$first, $second, $third];
+        // Reveal the sequence.
+        $sequence = array_map(function($item) { return $item->reveal(); }, $sequence);
+
+        $actionable = $this->tp()->reduceReportToActionableTradeProposal($sequence);
+
+        $this->assertNull($actionable);
+    }
+
+    /**
+     * @covers AppBundle\API\Bitstamp\TradePairs\BitstampTradePairs::reduceReportToActionableTradeProposal
+     *
+     * @group stable
+     */
+    public function testReduceReportToActionableTradeProposalPreValidFinal()
+    {
+        // Start with a valid mock.
+        $first = $this->statefulProposalMockRaw(true);
+        // Should be checked.
+        $first->isValid()->shouldBeCalled();
+        $first->isCompulsory()->shouldBeCalled();
+        $first->isFinal()->shouldBeCalled();
+
+        // Then a final mock.
+        $second = $this->statefulProposalMockRaw(true, false, true);
+        // Should be checked.
+        $second->isValid()->shouldBeCalled();
+        $second->isCompulsory()->shouldBeCalled();
+        $second->isFinal()->shouldBeCalled();
+
+        // Then something else.
+        $third = $this->statefulProposalMockRaw($this->randomBool(), $this->randomBool(), $this->randomBool());
+        // This should not be checked.
+        $third->isValid()->shouldNotBeCalled();
+        $third->isCompulsory()->shouldNotBeCalled();
+        $third->isFinal()->shouldNotBeCalled();
+
+        // Build the sequence.
+        $sequence = [$first, $second, $third];
+        // Reveal the sequence.
+        $sequence = array_map(function($item) { return $item->reveal(); }, $sequence);
+
+        $actionable = $this->tp()->reduceReportToActionableTradeProposal($sequence);
+
+        $this->assertSame($sequence[0], $actionable);
+    }
+
     /**
      * @covers AppBundle\API\Bitstamp\TradePairs\BitstampTradePairs::reduceReportToActionableTradeProposal
      *
@@ -137,6 +223,7 @@ class BitstampTradePairsTest extends WebTestCase
     {
         $pre = [
             // Valid pre-mocks have to be tested separately.
+            // @see testReduceReportToActionableTradeProposalPreValidFinal()
             [false],
         ];
         $post = [
@@ -150,6 +237,7 @@ class BitstampTradePairsTest extends WebTestCase
             [false, false, true],
         ];
         // Invalid finals have to be tested separately.
+        // @see testReduceReportToActionableTradeProposalInvalidFinal()
         $tests = [
             [true, true, true],
             [true, false, true],
