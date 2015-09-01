1816617986b606fe14ec0eacda1db51be61d778f
David Meister <thedavidmeister@gmail.com>
2015-09-02 00:57:11 +1000

    don't check CI for parameter setting

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -5,10 +5,5 @@ use AppBundle\Cast;
 
 $secrets = new Secrets();
 
-// Move environment variables to parameters if we are not in a CI environment.
-// In CI, we want all environment variables and similar to be set explicitly by
-// the test suite.
-if (!Cast::toBoolean($secrets->get('CI'))) {
-  $container->setParameter('bitstamp.price_proposer.min_max_step', [$secrets->get('BITSTAMP_PERCENTILE_MIN'), $secrets->get('BITSTAMP_PERCENTILE_MAX'), $secrets->get('BITSTAMP_PERCENTILE_STEP')]);
-}
-
+// Move environment variables to parameters.
+$container->setParameter('bitstamp.price_proposer.min_max_step', [$secrets->get('BITSTAMP_PERCENTILE_MIN'), $secrets->get('BITSTAMP_PERCENTILE_MAX'), $secrets->get('BITSTAMP_PERCENTILE_STEP')]);
