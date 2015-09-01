bde353d108797c143209fe1cea93dcee0f3ecef3
David Meister <thedavidmeister@gmail.com>
2015-09-02 01:02:55 +1000

    added comments to isBooleany

--- src/AppBundle/Ensure.php
+++ src/AppBundle/Ensure.php
@@ -18,6 +18,17 @@ final class Ensure
         // Do nothing.
     }
 
+    /**
+     * Ensure that a value is boolean-y.
+     *
+     * Boolean-y values follow PHP rules, 1/0, "yes"/"no", "true"/"false" and
+     * true/false are all boolean-y. Everything else is not.
+     *
+     * @param boolean-y $value
+     *   The value that may or may not be boolean-y.
+     *
+     * @return boolean-y $value
+     */
     public static function isBooleany($value)
     {
         return null !== filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) ? $value : fail('%s is not a boolean.');
