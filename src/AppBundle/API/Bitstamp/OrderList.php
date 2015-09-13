ae78707b45d2537a10a1036d4542e2b3bce9b9bb
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-09-13 10:46:30 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/AppBundle/API/Bitstamp/OrderList.php
+++ src/AppBundle/API/Bitstamp/OrderList.php
@@ -320,11 +320,11 @@ class OrderList
      */
     protected function percentileIndexCompare(Money $index, array $comparisons)
     {
-        v::each(v::instance('Money\Money'))->check(array_map(function ($item) {
+        v::each(v::instance('Money\Money'))->check(array_map(function($item) {
             return $item['percentile'];
         }, $comparisons));
 
-        v::each(v::int())->check(array_map(function ($item) {
+        v::each(v::int())->check(array_map(function($item) {
             return $item['usd'];
         }, $comparisons));
 
